7018fe308d6c12b2f4ecefa0e04a8568d9118de4
Dan Dowd <danieldowd89@gmail.com>
2024-07-22 21:17:10 -0600

    adds tutorial window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "vulkan/vulkan_core.h"
+#include <cstdint>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -10,4 +12,25 @@
 
 int main() {
   glfwInit();
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
+
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+  std::cout << extensionCount << " extensions supported\n";
+
+  glm::mat4 matrix;
+  glm::vec4 vec;
+  auto test = matrix *vec;
+
+  while(!glfwWindowShouldClose((window))) {
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
+
+  return 0;
 }
