8565729384795c8b451211487cb57d70cb2a7168
Dan Dowd <danieldowd89@gmail.com>
2024-07-24 09:32:18 -0600

    adds validation layer setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-#include "vulkan/vulkan_core.h"
-#include <cstdint>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+#include <cstdint>
+#include <cstring>
+#include <ostream>
 
 #include <exception>
 #include <iostream>
@@ -11,6 +13,16 @@
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+  "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+  const bool enableValidationLayers = false;
+#else
+  const bool enableValidationLayers = true;
+#endif
 
 class HelloTriangleApplication {
   public:
@@ -26,6 +38,7 @@ class HelloTriangleApplication {
     VkInstance instance;
 
     void initWindow() {
+      std::cout << enableValidationLayers << std::endl;
       glfwInit();
 
       glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -38,7 +51,36 @@ class HelloTriangleApplication {
       createInstance();
     }
 
+    bool checkValidationLayerSupport() {
+      uint32_t layerCount;
+      vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+      std::vector<VkLayerProperties> availableLayers(layerCount);
+      vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+      for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+
+        for (const auto& layerProperties : availableLayers) {
+          if (strcmp(layerName, layerProperties.layerName) == 0) {
+            layerFound = true;
+            break;
+          }
+        }
+
+        if (!layerFound) {
+          return false;
+        }
+      }
+
+      return true;
+    };
+
     void createInstance() {
+      if (enableValidationLayers && !checkValidationLayerSupport()) {
+        throw std::runtime_error("validation layers requested, but not available!");
+      }
+
       VkApplicationInfo appInfo {};
       appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
       appInfo.pApplicationName = "Hello Triangle";
@@ -69,6 +111,14 @@ class HelloTriangleApplication {
       createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
       createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
       createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+      if (enableValidationLayers) {
+        std::cout << "validation enabled, adding layers." << std::endl;
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+      } else {
+        createInfo.enabledLayerCount = 0;
+      }
 
       if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
         throw std::runtime_error("failed to create instance!");
